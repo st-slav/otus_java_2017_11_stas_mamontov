158e9f58151eec98c577fee56a6d5a42186b609d
st-slav <st.mam_@hotmail.com>
2017-11-21 14:05:47 +0500

    created project

---
+++ block_first/lesson-first/src/main/java/ru/mamsta/otus/blockfirst/lessonfirst/Main.java
@@ -0,0 +1,10 @@
+package ru.mamsta.otus.blockfirst.lessonfirst;
+
+import com.google.common.base.MoreObjects;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(MoreObjects.class.getCanonicalName());
+    }
+}
